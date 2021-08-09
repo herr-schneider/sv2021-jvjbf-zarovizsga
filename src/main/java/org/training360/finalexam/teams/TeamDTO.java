bd123d7cab53e7c8eaedabc9acb2eecbb8bf965c
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-08-09 16:52:35 +0200

    A TeamDTO nem PlayerDTO volt, ez egy hiba

--- src/main/java/org/training360/finalexam/teams/TeamDTO.java
+++ src/main/java/org/training360/finalexam/teams/TeamDTO.java
@@ -1,8 +1,10 @@
 package org.training360.finalexam.teams;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.training360.finalexam.players.Player;
+import org.training360.finalexam.players.PlayerDTO;
 
 import javax.persistence.*;
 import java.util.List;
@@ -20,10 +22,9 @@ public class TeamDTO {
     private String name;
 
     @OneToMany(cascade = {PERSIST, REMOVE}, mappedBy = "team")
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    //@JsonBackReference
-    @JsonIgnore
-    private List<Player> players;
+//    @EqualsAndHashCode.Exclude
+//    @ToString.Exclude
+//    @JsonBackReference
+    private List<PlayerDTO> players;
 
 }
